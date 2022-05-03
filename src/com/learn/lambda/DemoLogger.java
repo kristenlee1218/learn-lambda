2a1c24469732eee5dd5aea44a90216267fee7be5
kristen <1065200748@qq.com>
2022-05-03 22:24:25 +0800

    2022.5.3

---
+++ src/com/learn/lambda/DemoLogger.java
@@ -0,0 +1,8 @@
+package com.learn.lambda;
+
+public class DemoLogger {
+    public static void main(String[] args) {
+        // Logger logger = LogManager.getLogger(DemoLogger.class);
+        // logger.debug(() -> "这是一个debug日志");
+    }
+}
--- src/com/learn/lambda/Test.java
+++ src/com/learn/lambda/Test.java
@@ -1,8 +1,5 @@
 package com.learn.lambda;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author : Kristen
  * @date : 2022/4/23
